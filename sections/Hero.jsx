e47e45d386e63750877d3832e337b086f3948614
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-04 18:07:27 -0400

    finished button for seeing more work and half of the hero page

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 import { words } from '../src/constants'
+import Button from '../src/components/Button'
 
 const Hero = () => {
   return (
     <section>
-        <div className='absolute top-0 left-0 z-10'>
-            <img src='/images/bg.png' alt='background'/>
-        </div>
+      <div className='absolute top-0 left-0 z-10'>
+        <img src='/images/bg.png' alt='background' />
+      </div>
 
-        <div className='hero-layout'>
-            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-                <div className='flex flex-col gap-7'>
-                    <div className='hero-text'>
-                        <h1>Shaping 
-                            <span className='"slide'>
-                                <span className='wrapper'>
-                                    {words.map((words) => (
-                                        <span key={words.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                                            <img src={words.imgPath} alt={words.text} className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'/>
-                                        </span>
-                                    ))}
-                                </span>
-                                <span> {words.text}</span>
-                            </span>
-                        </h1>
-                        <h1> Into Real Projects</h1>
-                        <h1>that deliver Results</h1>
-                    </div>
-                </div>
-            </header>
-        </div>
+      <div className='hero-layout'>
+        <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+          <div className='flex flex-col gap-7'>
+            <div className='hero-text'>
+              <h1>
+                Shaping{' '}
+                <span className='slide'>
+                  <span className='wrapper'>
+                    {words.map((word) => (
+                      <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>Into Real Projects</h1>
+              <h1>that deliver Results</h1>
+            </div>
+            <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
+              Hi I'm Jacob, a developer based in New York with a passion for coding
+            </p>
+            <Button className="md:w-80 md:h-16 w-60 h-12" id="counter" text="See my Work"/>
+          </div>
+        </header>
+      </div>
     </section>
   )
 }
