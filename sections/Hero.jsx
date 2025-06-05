3783672d769b675444aba052d9a63fc3c384be48
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-04 23:36:12 -0400

    added gsap

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import { words } from '../src/constants'
 import Button from '../src/components/Button'
 import HeroExperience from '../src/components/HeroModels/HeroExperience'
-
+import { useGSAP  } from '@gsap/react'
+import gsap from 'gsap'
 const Hero = () => {
+
+  useGSAP(() => {
+    gsap.fromTo('.hero-text h1', 
+      {
+        y:50,
+        opacity: 0
+      },
+      {
+        
+      }
+    )
+  })
   return (
     <section>
       <div className='absolute top-0 left-0 z-10'>
