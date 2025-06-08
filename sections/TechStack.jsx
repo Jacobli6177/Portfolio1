49467f399329539535b611c80ad8417c986945bc
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-07 23:43:15 -0400

    full implementing Techstack with names

--- sections/TechStack.jsx
+++ sections/TechStack.jsx
@@ -27,12 +27,15 @@ const TechStack = () => {
         sub= "The skills I bring to the Table"
         />
         <div className='tech-grid'>
-            {techStackIcons.map((icon) => (
-                <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
+            {techStackIcons.map((techStackIcons) => (
+                <div key={techStackIcons.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
                     <div className='tech-card-animated-bg'/>
                     <div className='tech-card-content'>
                         <div className='tech-icon-wrapper'>
-                          <TechIcon model={icon}/>
+                          <TechIcon model={techStackIcons}/>
+                        </div>
+                        <div className='padding-x w-full'>
+                          <p>{techStackIcons.name}</p>
                         </div>
                     </div>
                 </div>
