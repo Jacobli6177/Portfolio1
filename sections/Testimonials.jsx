fdfaf07b0dbba800f7e0d1e6505900da6e6f6c1a
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-08 20:51:35 -0400

    finished testimonials

--- sections/Testimonials.jsx
+++ sections/Testimonials.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import TitleHeader from '../src/components/TitleHeader'
+import { testimonials } from '../src/constants'
+import { GlowCard } from '../src/components/HeroModels/GlowCard';
 
 const Testimonials = () => {
   return (
-    <div>
-      testimonials 
-    </div>
+    <section id="testimonials" className='flex-center section-padding'>
+      <div className='w-full h-full md:px-10 px-5'>
+        <TitleHeader title="What People say about me" sub="Client FeedBack Highlights"/>
+        <div className='lg:columns-3 md:columns-2 columns-1 mt-16'> 
+          {testimonials.map(( { imgPath, name, mentions, review }) => (
+            <GlowCard card= { { review }}>
+              <div className='flex items-center gap-3'>
+                <img src={imgPath} alt={name}/>
+              </div>
+              <div>
+                <p className='font-bold'> {name} </p>
+                <p className='text-white-50'> {mentions} </p>
+              </div>
+            </GlowCard>
+
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
