83c853350a4a116b54aa19f503022b9a04fb14c0
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-08 22:16:54 -0400

    Implemented footer + neeed to fix email public key

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -33,9 +33,9 @@ const Contact = () => {
       // Reset form and stop loading
       setForm({ name: "", email: "", message: "" });
     } catch (error) {
-      console.error("EmailJS Error:", error); // Optional: show toast
+      console.error("EmailJS Error:", error); 
     } finally {
-      setLoading(false); // Always stop loading, even on error
+      setLoading(false); 
     }
   };
 
