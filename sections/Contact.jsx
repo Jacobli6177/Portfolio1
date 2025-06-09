a336db5bdb667e6e4051ccad915154857308852e
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-09 16:18:00 -0400

    fixed contacting infromation sending

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -22,12 +22,16 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true); // Show loading state
 
+    console.log("Service ID:", import.meta.env.VITE_APP_EMAILJS_SERVICE);
+    console.log("Template ID:", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
+    console.log("Public Key:", import.meta.env.VITE_APP_EMAILJS_PUBLIC_ID);
+
     try {
       await emailjs.sendForm(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_ID
       );
 
       // Reset form and stop loading
